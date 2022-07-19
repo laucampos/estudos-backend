712f1919163e1562cd30e00ef41190ff4a5dbfe6
Laura <laura.nunes@labenu.com.br>
2022-07-19 17:06:57 -0300

    corrigido o filtro de buscas do GET

--- query-builders/src/endpoints/getPerfumes.ts
+++ query-builders/src/endpoints/getPerfumes.ts
@@ -6,18 +6,41 @@ export const getPerfumes = async (req: Request, res: Response) => {
     let errorCode = 400;
 
     try {
-        const query = req.query.q;
+        const name = req.query.name;
+        const brand = req.query.brand;
         const sort = req.query.sort || "name";
         const order = req.query.order || "asc";
         const limit = Number(req.query.limit) || 10;
         const page = Number(req.query.page) || 1;
         const offset = limit * (page - 1);
 
-        if (query) {
+        if (name && brand) {
             const result = await connection(TABLE_PERFUMES)
                 .select()
-                .where("name", "LIKE", `%${query}%`)
-                .orWhere("brand", "LIKE", `%${query}%`)
+                .where("name", "LIKE", `%${name}%`)
+                .orWhere("name", "LIKE", `%${brand}%`)
+                .orderBy(`${sort}`, `${order}`)
+                .limit(limit)
+                .offset(offset);
+
+            return res.status(200).send({ perfumes: result });
+        }
+
+        if (name) {
+            const result = await connection(TABLE_PERFUMES)
+                .select()
+                .where("name", "LIKE", `%${name}%`)
+                .orderBy(`${sort}`, `${order}`)
+                .limit(limit)
+                .offset(offset);
+
+            return res.status(200).send({ perfumes: result });
+        }
+
+        if (brand) {
+            const result = await connection(TABLE_PERFUMES)
+                .select()
+                .where("brand", "LIKE", `%${brand}%`)
                 .orderBy(`${sort}`, `${order}`)
                 .limit(limit)
                 .offset(offset);
