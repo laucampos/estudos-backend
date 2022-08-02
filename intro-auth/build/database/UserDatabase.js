f8ace083b61855636d6e0123cd54b5cbbcc9ee37
Laura <laura.nunes@labenu.com.br>
2022-08-02 14:48:43 -0300

    exercícios feitos

---
+++ intro-auth/build/database/UserDatabase.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserDatabase = void 0;
+const BaseDatabase_1 = require("./BaseDatabase");
+class UserDatabase extends BaseDatabase_1.BaseDatabase {
+}
+exports.UserDatabase = UserDatabase;
+UserDatabase.TABLE_USERS = "Auth_Users";
+//# sourceMappingURL=UserDatabase.js.map
